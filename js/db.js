a719a6b3528604bf524671aab4c45ed846f395b4
alef050901 <55445170+alef050901@users.noreply.github.com>
2019-11-21 08:01:58 -0300

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,19 +1,46 @@
-
+// habilitar dados offline
 db.enablePersistence()
     .catch(function(err) {
         if (err.code == 'failed-precondition') {
-            console.log('persistance failed');
+            // provavelmente multiplas abas abertas ao mesmo tempo
+            console.log('Persistencia de dados falhou');
         } else if (err.code == 'unimplemented') {
-            console.log('persistance not available');
+            // browser nao suporta
+            console.log('Persistencia nao disponivel');
         }
     });
 
+// real-time listener que verifica as mudanças que ocorrem
 db.collection('comidas').onSnapshot(snapshot => {
     snapshot.docChanges().forEach(change => {
         if (change.type === 'added') {
-            renderRecipe(change.doc.data(), change.doc.id);
+            desenhaCard(change.doc.data(), change.doc.id);
         }
         if (change.type === 'removed') {
+            // remover da pagina tambem
         }
     });
+});
+
+// adicionar nova comida
+const form = document.querySelector('form');
+form.addEventListener('submit', evt => {
+    evt.preventDefault();
+
+    const comida = {
+        nome: form.comidaTitulo.value,
+        descricao: form.comidaDescricao.value,
+        link: form.comidaLink.value,
+        endereco_imagem: form.comidaArquivo.value
+    };
+
+    db.collection('comidas').add(comida)
+        .catch(err => console.log(err));
+
+    //reseta o formulario
+    form.comidaTitulo.value = '';
+    form.comidaDescricao.value = '';
+    form.comidaLink.value = '';
+    form.comidaArquivo.value = '';
+
 });
